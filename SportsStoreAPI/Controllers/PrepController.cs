8e9261164c08ee5238fb0632502c25e60d6dd6bc
digitopoi <hubo85@gmail.com>
2017-11-14 21:27:52 -0600

    Update test controller to test authentication

--- SportsStoreAPI/Controllers/PrepController.cs
+++ SportsStoreAPI/Controllers/PrepController.cs
@@ -1,6 +1,12 @@
 ﻿using System.Threading.Tasks;
 using System.Web.Mvc;
 using SportsStoreAPI.Models;
+using SportsStoreAPI.Infrastructure.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Web;
+using System.Security.Claims;
 
 namespace SportsStoreAPI.Controllers
 {
@@ -18,12 +24,14 @@ namespace SportsStoreAPI.Controllers
             return View(repo.Products);
         }
 
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> DeleteProduct(int id)
         {
             await repo.DeleteProductAsync(id);
             return RedirectToAction("Index");
         }
 
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> SaveProduct(Product product)
         {
             await repo.SaveProductAsync(product);
@@ -45,6 +53,23 @@ namespace SportsStoreAPI.Controllers
         {
             await repo.SaveOrderAsync(order);
             return RedirectToAction("Orders");
+        }
+
+        public async Task<ActionResult> SignIn()
+        {
+            IAuthenticationManager authMgr = HttpContext.GetOwinContext().Authentication;
+            StoreUserManager userMgr = HttpContext.GetOwinContext().GetUserManager<StoreUserManager>();
+
+            StoreUser user = await userMgr.FindAsync("Admin", "secret");
+            authMgr.SignIn(await userMgr.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie));
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut();
+            return RedirectToAction("Index");
         }
     }
 }
