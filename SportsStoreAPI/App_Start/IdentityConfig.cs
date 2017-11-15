2b6f4f8eaf10bbc7a19154c4bccdd4548c69c596
digitopoi <hubo85@gmail.com>
2017-11-14 19:35:32 -0600

    Add identity config

---
+++ SportsStoreAPI/App_Start/IdentityConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SportsStoreAPI.IdentityConfig))]
+
+namespace SportsStoreAPI
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app) { }
+    }
+}
