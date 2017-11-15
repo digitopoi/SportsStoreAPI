c51a0432fa89d3d8d584d37603264e42093f5e39
digitopoi <hubo85@gmail.com>
2017-11-14 20:47:52 -0600

    Add store user identity

---
+++ SportsStoreAPI/Infrastructure/Identity/StoreUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SportsStoreAPI.Infrastructure.Identity
+{
+    public class StoreUser : IdentityUser
+    {
+        //  application specific properties go here
+    }
+}
