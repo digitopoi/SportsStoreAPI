4ca8c4c599b06dc7e69abe6e6dc36cc801d34a02
digitopoi <hubo85@gmail.com>
2017-11-14 19:48:23 -0600

    add db context

---
+++ SportsStoreAPI/Models/ProductDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SportsStoreAPI.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext() : base("SportsStoreDb")
+        {
+            Database.SetInitializer<ProductDbContext>(new ProductDbInitializer());
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+    }
+}
