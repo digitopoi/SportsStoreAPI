282da0c0f12990abecdb3920cabbebd1395a116a
digitopoi <hubo85@gmail.com>
2017-11-14 20:52:00 -0600

    Add role identity

---
+++ SportsStoreAPI/Infrastructure/Identity/StoreRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SportsStoreAPI.Infrastructure.Identity
+{
+    public class StoreRole : IdentityRole
+    {
+        public StoreRole() : base() { }
+        public StoreRole(string name) : base(name) { }
+    }
+}
